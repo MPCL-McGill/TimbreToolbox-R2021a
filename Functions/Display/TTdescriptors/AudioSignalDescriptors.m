1d0a6baa6c23f3d9dddde06693c52b9aefc14683
Eddy <71031534+EddySavvasKazazis@users.noreply.github.com>
2021-09-17 19:35:36 +0300

    Add files via upload
    Updated Frame Energy, RMS Energy

---
+++ Functions/Display/TTdescriptors/AudioSignalDescriptors.m
@@ -0,0 +1,11 @@
+function [] = AudioSignalDescriptors
+
+fprintf('Descriptors available to the ASrep: \n\n');
+
+disp('ALL   : computes all descriptors')
+disp('zeroCrossingRate')
+disp('frameEnergy')
+disp('rmsEnergy')
+disp('autocorrelationCoefficients')
+
+fprintf('\n\n')
