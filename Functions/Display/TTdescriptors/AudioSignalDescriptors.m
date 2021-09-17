4881cf0d3d2ee5bd5639e5802e2606dc24f3f4f5
Eddy <71031534+EddySavvasKazazis@users.noreply.github.com>
2021-09-17 19:34:37 +0300

    Delete AudioSignalDescriptors.m

--- Functions/Display/TTdescriptors/AudioSignalDescriptors.m
+++
@@ -1,9 +0,0 @@
-function [] = AudioSignalDescriptors
-
-fprintf('Descriptors available to the ASrep: \n\n');
-
-disp('ALL   : computes all descriptors')
-disp('zeroCrossingRate')
-disp('autocorrelationCoefficients')
-
-fprintf('\n\n')
